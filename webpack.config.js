96bf739d818130fcb5eaf47ec0f68b7d2ced6f4d
Saqlen Raza Mehdi <saqlenrazamehdi@gmail.com>
2023-06-24 11:33:50 +0500

    finalized the package.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    libraryTarget: "umd",
+    library: "useAudio",
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
